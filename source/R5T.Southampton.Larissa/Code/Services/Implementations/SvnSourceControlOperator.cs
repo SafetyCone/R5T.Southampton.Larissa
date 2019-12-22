ab180d089474f9aafcf19e1e787e79f0deeeccdf
MinexAutomation <minex.automation@gmail.com>
2019-12-21 22:44:17 -0800

    Added ISvnversionOperator, ILogger, and implemented Add(), Checkout(), and CommitToRemote().

--- source/R5T.Southampton.Larissa/Code/Services/Implementations/SvnSourceControlOperator.cs
+++ source/R5T.Southampton.Larissa/Code/Services/Implementations/SvnSourceControlOperator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using Microsoft.Extensions.Logging;
+
 using R5T.Larissa;
 
 
@@ -8,26 +10,30 @@ namespace R5T.Southampton.Larissa
     public class SvnSourceControlOperator : ISourceControlOperator
     {
         private ISvnOperator SvnOperator { get; }
+        private ISvnversionOperator SvnversionOperator { get; }
+        private ILogger Logger { get; }
 
 
-        public SvnSourceControlOperator(ISvnOperator svnOperator)
+        public SvnSourceControlOperator(ISvnOperator svnOperator, ISvnversionOperator svnversionOperator, ILogger<SvnSourceControlOperator> logger)
         {
             this.SvnOperator = svnOperator;
+            this.SvnversionOperator = svnversionOperator;
+            this.Logger = logger;
         }
 
         public void Add(string path)
         {
-            throw new NotImplementedException();
+            this.SvnOperator.Add(path);
         }
 
         public void Checkout(string repositoryUrl, string localDirectoryPath)
         {
-            throw new NotImplementedException();
+            this.SvnOperator.Checkout(repositoryUrl, localDirectoryPath);
         }
 
         public void CommitToRemote(string path, string message)
         {
-            throw new NotImplementedException();
+            this.SvnOperator.Commit(path, message);
         }
 
         public string GetRemoteRepositoryUrl(string path)
@@ -48,6 +54,20 @@ namespace R5T.Southampton.Larissa
         public void Update(string path)
         {
             throw new NotImplementedException();
+        }
+
+        public SourceControlOperatorInformation GetOperatorInformation()
+        {
+            var description = "SVN";
+            var version = this.SvnOperator.GetVersion();
+
+            var operatorInformation = new SourceControlOperatorInformation()
+            {
+                Description = description,
+                Version = version,
+            };
+
+            return operatorInformation;
         }
     }
 }
